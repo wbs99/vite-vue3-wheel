5c7cf0688861eb4064577498886572927fcb6880
dingtalk_xsgrpl <1134954328@qq.com>
2022-07-29 21:54:11 +0800

    路由切换关闭 aside

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>buttonDemo</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,28 @@ import { createWebHashHistory, createRouter, RouteRecordRaw } from "vue-router";
 
 const routes = <Array<RouteRecordRaw>>[
   { path: "/", component: () => import("../views/Home.vue") },
-  { path: "/doc", component: () => import("../views/Doc.vue") },
+  {
+    path: "/doc",
+    component: () => import("../views/Doc.vue"),
+    children: [
+      {
+        path: "switch",
+        component: () => import("../components/SwitchDemo.vue"),
+      },
+      {
+        path: "button",
+        component: () => import("../components/ButtonDemo.vue"),
+      },
+      {
+        path: "dialog",
+        component: () => import("../components/DialogDemo.vue"),
+      },
+      {
+        path: "tabs",
+        component: () => import("../components/TabsDemo.vue"),
+      },
+    ],
+  },
 ];
 const router = createRouter({
   history: createWebHashHistory(),
